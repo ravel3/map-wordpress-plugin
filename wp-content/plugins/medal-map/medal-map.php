117c17788a014216e4da4ed6790ce9f78358f62f
ravel <john.doe@example.com>
2025-07-07 14:08:20 +0200

    removed some commented out code + plugin renaming

--- wp-content/plugins/medal-map/medal-map.php
+++ wp-content/plugins/medal-map/medal-map.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * Plugin Name: Medal Map Database System
- * Plugin URI: https://example.com/medal-map
- * Description: Interaktywne mapy medalów z wykorzystaniem bazy danych WordPress dla wielu map
- * Version: 2.0.0
- * Author: Medal Map System
- * License: GPL v2 or later
+ * Plugin Name: Hawran Medal Map
+ * Version: 1.0.0
+ * Author: Artur Flis
  * Text Domain: medal-map
  */
 
@@ -15,7 +12,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Stałe pluginu
-define('MEDAL_MAP_VERSION', '2.0.0');
+define('MEDAL_MAP_VERSION', '1.0.0');
 define('MEDAL_MAP_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('MEDAL_MAP_PLUGIN_PATH', plugin_dir_path(__FILE__));
 require_once MEDAL_MAP_PLUGIN_PATH . 'includes/class-database.php';
@@ -24,25 +21,26 @@ require_once MEDAL_MAP_PLUGIN_PATH . 'includes/class-database.php';
 /**
  * Główna klasa pluginu Medal Map
  */
-class MedalMapPlugin {
+class MedalMapPlugin
+{
 
     private static $instance = null;
 
-    public static function get_instance() {
+    public static function get_instance()
+    {
         if (null === self::$instance) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
-    private function __construct() {
+    private function __construct()
+    {
         add_action('init', array($this, 'init'));
-//        register_activation_hook(__FILE__, array($this, 'activate'));
-//        register_deactivation_hook(__FILE__, array($this, 'deactivate'));
-//        register_uninstall_hook(__FILE__, array('MedalMapPlugin', 'uninstall'));
     }
 
-    public function init() {
+    public function init()
+    {
         // Ładowanie plików językowych
         load_plugin_textdomain('medal-map', false, dirname(plugin_basename(__FILE__)) . '/languages');
 
@@ -59,7 +57,8 @@ class MedalMapPlugin {
         new Medal_Map_Shortcode();
     }
 
-    private function includes() {
+    private function includes()
+    {
         require_once MEDAL_MAP_PLUGIN_PATH . 'includes/class-frontend.php';
         require_once MEDAL_MAP_PLUGIN_PATH . 'includes/class-ajax.php';
         require_once MEDAL_MAP_PLUGIN_PATH . 'includes/class-shortcode.php';
@@ -69,7 +68,8 @@ class MedalMapPlugin {
         }
     }
 
-    public function activate() {
+    public function activate()
+    {
         // Tworzenie tabel bazy danych
         Medal_Map_Database::create_tables();
 
@@ -77,11 +77,13 @@ class MedalMapPlugin {
         flush_rewrite_rules();
     }
 
-    public function deactivate() {
+    public function deactivate()
+    {
         flush_rewrite_rules();
     }
 
-    public static function uninstall() {
+    public static function uninstall()
+    {
         // Usuwanie tabel (opcjonalne)
         if (get_option('medal_map_delete_data_on_uninstall', false)) {
             Medal_Map_Database::drop_tables();
